24930cc2565375fb813f2ad3efeed142c4f47c14
Dennis <dennisrexhepi@gmail.com>
2026-01-08 12:56:04 +0100

    robetta nuova

--- wordpress/wp-content/wflogs/config.php
+++ wordpress/wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:5:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1766744629;s:7:"authKey";s:64:";GRhqDE,-$1Q|_>7Dl-J8Vr;fXr#?Dz8g;c0MSXcY>$Ag_C;Q*yE|}y KTmV~[$a";s:7:"version";s:5:"1.1.0";}
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:";GRhqDE,-$1Q|_>7Dl-J8Vr;fXr#?Dz8g;c0MSXcY>$Ag_C;Q*yE|}y KTmV~[$a";s:7:"version";s:5:"1.1.0";s:13:"attackDataKey";i:1970;s:11:"wafDisabled";b:1;}
