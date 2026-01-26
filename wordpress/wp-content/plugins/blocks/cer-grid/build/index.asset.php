30af62f97ba4f93c151ec67888bdef3cbe9b8882
Dennis <dennisrexhepi@gmail.com>
2026-01-26 17:50:18 +0100

    controlla il repo di andrea perchè la griglia non va

--- wordpress/wp-content/plugins/blocks/cer-grid/build/index.asset.php
+++ wordpress/wp-content/plugins/blocks/cer-grid/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => 'b441bc73a18d25f0d763');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '25451361b2629234ad9e');
--- wordpress/wp-content/plugins/blocks/cer-grid/src/index.js
+++ wordpress/wp-content/plugins/blocks/cer-grid/src/index.js
@@ -2,18 +2,50 @@ import {registerBlockType} from '@wordpress/blocks';
 //tra graffe indico il nome della funz da importare. guarda anche export default per info aggiuntive
 //controlla l'importazione e non fidarti di copilot, magari vuoi importare qualcosa di omonimo però da un altro pacchetto
 import blockMeta from '../block.json'; //importa il file block.json e lo assegna alla variabile blockMeta (blockMeta nome che decido io)
-import{InnerBlocks, useBlockProps} from '@wordpress/block-editor';
+import{InnerBlocks, useBlockProps, InspectorControls} from '@wordpress/block-editor';
+import{PanelBody, RangeControl} from '@wordpress/components';
 
 registerBlockType(blockMeta.name, /*prende il nome del blocco dal file block.json*/ {
         title: blockMeta.title,
         description: blockMeta.description,
         category: blockMeta.category,
         icon: blockMeta.icon,
+        attributes:{
+            "columns":{
+                "type": "number",
+                "default": 3
+            }
+
+        },
         edit: (props) =>{
-            const blockProps= useBlockProps() //per sfruttare le funzionalità native di wp
-            return <div {...blockProps}>
-                <InnerBlocks />
-            </div>
+            const blockProps=useBlockProps({
+                style: {
+                    '--cer-grid--columns': props.attributes.columns
+                }
+            });
+            
+            return <>
+                <InspectorControls key="settings">
+                    <PanelBody title="Grid settings">
+                        <RangeControl
+                            label="numero di colonne"
+                            value={props.attributes.columns}
+                            onChange={
+                                (newColumns) => {props.setAttributes({columns : newColumns})}
+                            }
+                            min={1}
+                            max={12}
+                            step={1}
+                            withInputField={true}
+                            
+                            />
+                    </PanelBody>
+
+                </InspectorControls>
+                <div {...blockProps}>
+                    <InnerBlocks />
+                </div>
+            </>
         },
         /*funzione freccia
         posso fare anche un file edit.js e save.js e le importo; 
@@ -25,10 +57,18 @@ registerBlockType(blockMeta.name, /*prende il nome del blocco dal file block.jso
             return <span>{date.toString()}</span>
             */
         save: (props) =>{
-            const blockProps= useBlockProps.save(props);
-                return <div {...blockProps}>
-                    <InnerBlocks.Content />
-                </div>
+            
+            const blockProps= useBlockProps.save({
+                style: {
+                    '--cer-grid--columns': props.attributes.columns
+                }
+                });
+                return <>
+
+                    <div {...blockProps}>
+                        <InnerBlocks.Content />
+                    </div>
+                </>
         }
         //codice che viene eseguito quando pigio salva, entrambe devono aver eun return. il return deve avere solo 1 tag
         //InnerBlocks permette di inserire altri blocchi dentro questo blocco ma posso usarlo solo una volta
